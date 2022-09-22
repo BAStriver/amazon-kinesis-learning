7234f033c3a754d8065f2f7c9c1a8e79acc11062
Striver <1312395217@qq.com>
2022-09-22 22:35:55 +0800

    update aws kinesis logic

--- src/main/java/com/amazonaws/services/kinesis/samples/stocktrades/processor/StockTradesProcessor.java
+++ src/main/java/com/amazonaws/services/kinesis/samples/stocktrades/processor/StockTradesProcessor.java
@@ -15,20 +15,24 @@
 
 package com.amazonaws.services.kinesis.samples.stocktrades.processor;
 
-import java.util.UUID;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
+import lombok.val;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
+import software.amazon.awssdk.auth.credentials.*;
+import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
-import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient;
 import software.amazon.awssdk.services.kinesis.KinesisAsyncClient;
-import software.amazon.kinesis.common.ConfigsBuilder;
+import software.amazon.awssdk.services.kinesis.KinesisBaseClientBuilder;
+import software.amazon.awssdk.services.kinesis.KinesisClient;
+import software.amazon.awssdk.services.kinesis.model.ListStreamsResponse;
+import software.amazon.awssdk.services.kinesis.model.PutRecordRequest;
+import software.amazon.awssdk.services.kinesis.model.PutRecordResponse;
 import software.amazon.kinesis.common.KinesisClientUtil;
-import software.amazon.kinesis.coordinator.Scheduler;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Uses the Kinesis Client Library (KCL) 2.2.9 to continuously consume and process stock trade
@@ -66,43 +70,36 @@ public class StockTradesProcessor {
     }
 
     public static void main(String[] args) throws Exception {
-        checkUsage(args);
 
+        // aws sts assume-role --role-arn arn:aws:iam::962862317094:role/DeveloperRole --role-session-name bas-developer --profile developer
+        // aws kinesis list-streams --profile developer --region us-west-1
         setLogLevels();
 
-        String applicationName = args[0];
-        String streamName = args[1];
-        Region region = Region.of(args[2]);
+        String streamName = "StockTradeStream";
+        Region region = Region.of("us-west-1");
 
-        if (region == null) {
-            System.err.println(args[2] + " is not a valid AWS region.");
-            System.exit(1);
-        }
+//        System.setProperty("aws.accessKeyId", "ASIA61LYQ7YTFYX47H4H");
+//        System.setProperty("aws.secretKey", "IxLe7U5K1JVM71TpLWsxBeuGMEWZ9Z7GAmDobRX3");
+        AwsSessionCredentials awsSessionCredentials = AwsSessionCredentials.create("ASIA61LYQAW5FYX37H4H", "IxLe7U1K5JVM7456GWsxBeuGMEWZ9Z7GAmDobRX3",
+                "FwoGZXIvYXdzEMn//////////wEaDGuJQC41JRTaTsaM7CKxAS72n7pfajAUFm8tdQhRwyr3/m2LYNmRQh88Yl+g4Y+YLunVBx6gnBreKOD9q2sKD3r6MRvOw6jD3FSZyznwpBVTYGGwzP/Lau3SRadeioXW7wEVAPGyNLd8fUZYuOzSqPc4j4ddUDrvu1so6D2ftDzgTAY2rfqtfEXALqmxbWu2Why1fHS7TOxYMjSoM54Qu4XqCa+MYA/M0FbT4JNJVbmYYxpq2xNkcOrJ4kvG9gDSnCiYuKeZBjItxIm6Rn3mUaxJR1MA7BHuLdV4F6V2F/lfB6oAaxM5j3p82sLMdB8n4MjcaPm6");
 
-        KinesisAsyncClient kinesisClient = KinesisClientUtil.createKinesisAsyncClient(KinesisAsyncClient.builder().region(region));
-        DynamoDbAsyncClient dynamoClient = DynamoDbAsyncClient.builder().region(region).build();
-        CloudWatchAsyncClient cloudWatchClient = CloudWatchAsyncClient.builder().region(region).build();
-        StockTradeRecordProcessorFactory shardRecordProcessor = new StockTradeRecordProcessorFactory();
-        ConfigsBuilder configsBuilder = new ConfigsBuilder(streamName, applicationName, kinesisClient, dynamoClient, cloudWatchClient, UUID.randomUUID().toString(), shardRecordProcessor);
+        DefaultCredentialsProvider defaultCredentialsProvider = DefaultCredentialsProvider.builder().profileName("developer").build();
 
-        Scheduler scheduler = new Scheduler(
-                configsBuilder.checkpointConfig(),
-                configsBuilder.coordinatorConfig(),
-                configsBuilder.leaseManagementConfig(),
-                configsBuilder.lifecycleConfig(),
-                configsBuilder.metricsConfig(),
-                configsBuilder.processorConfig(),
-                configsBuilder.retrievalConfig()
-        );
-        int exitCode = 0;
-        try {
-            scheduler.run();
-        } catch (Throwable t) {
-            LOG.error("Caught throwable while processing data.", t);
-            exitCode = 1;
-        }
-        System.exit(exitCode);
+        KinesisAsyncClient kinesisClient = KinesisClientUtil.createKinesisAsyncClient(
+                KinesisAsyncClient.builder().region(region)
+                        .credentialsProvider(defaultCredentialsProvider)
+                /*.credentialsProvider(StaticCredentialsProvider.create(awsSessionCredentials))*/);
 
+        CompletableFuture<ListStreamsResponse> listStreamsResponseCompletableFuture = kinesisClient.listStreams();
+        System.out.println(listStreamsResponseCompletableFuture.get());
+
+        PutRecordRequest request = PutRecordRequest.builder()
+                .partitionKey(UUID.randomUUID().toString()) // We use the ticker symbol as the partition key, explained in the Supplemental Information section below.
+                .streamName(streamName)
+                .data(SdkBytes.fromByteArray("BAS".getBytes()))
+                .build();
+        CompletableFuture<PutRecordResponse> result = kinesisClient.putRecord(request);
+        System.out.println(result.get());
     }
 
 }
